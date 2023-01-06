0fb3ec59fceffb7588f360bee59e0b7283a51153
Smahle <stianmahle@gmail.com>
2023-01-06 01:44:21 +0100

    sidenav

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageModule } from './homepage/homepage.module';
@@ -18,7 +18,8 @@ import { PersonFormModule } from './person-form/person-form.module';
     PersonFormModule,
     HomepageModule,
     OverviewModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
